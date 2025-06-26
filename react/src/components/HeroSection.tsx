d2e8e611d7b49b4c5675c9a27476445a75b99832
deepshah995 <deepshah995>
2025-06-26 19:27:27 -0400

    Main changes

--- react/src/components/HeroSection.tsx
+++ react/src/components/HeroSection.tsx
@@ -44,11 +44,11 @@ const HeroSection = () => {
                     transition={{ duration: 0.8, delay: 0.6 }}
                     {...{className:"md:w-1/2 bg-[#17072b] bg-opacity-70 p-8 rounded-2xl shadow-lg text-center max-w-md md:ml-12 my-8 md:my-12"}}
                 >
-                    <h3 className="text-2xl md:text-3xl font-bold text-white mb-2">
+                    <h3 className="text-xl md:text-2xl font-medium text-white mb-2">
                         Upcoming Webinar:
                     </h3>
                     <h3 className="text-2xl md:text-3xl font-bold text-white mb-4">
-                        AI Roadmap for Teens!
+                        AI Roadmap for Teens !
                     </h3>
                     <p className="text-lg md:text-xl text-gray-300 mb-6">
                         <span className="text-teal-300 text-2xl font-bold">Aug 2nd, 2025 | Saturday</span>
